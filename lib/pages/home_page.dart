123ffe452338901ea6675f9849de446a456b3d8d
tann325 <intandwifeb47@gmail.com>
2025-09-09 10:46:54 +0700

    Struktur Folder oleh

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,8 +12,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final List<Pet> pets = [
     Pet(
-      name: "Mondy",
-      type: "Kucing",
+      name: "kucing",
+      type: "mondy",
       imagePath: "assets/kucing.jpg",
       description: "Mondy adalah kucing yang sangat manja.",
       caretips:
