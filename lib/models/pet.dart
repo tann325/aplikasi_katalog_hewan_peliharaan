65beb3c912769e6d65cb8c1175be32a88b19e2bb
Sonyabella <sonyaxpplg1@gmail.com>
2025-09-09 09:39:18 +0700

    intan commit

---
+++ lib/models/pet.dart
@@ -0,0 +1,15 @@
+class Pet {
+  final String name;
+  final String type;
+  final String imagePath;
+  final String description;
+  final String caretips;
+
+  Pet({
+    required this.name,
+    required this.type,
+    required this.imagePath,
+    required this.description,
+    required this.caretips,
+  });
+}
